908b684ea4e9cf8fd9a11a69ebb4b3c233defebe
S3dany <s3dany88@gmail.com>
2020-10-04 19:05:06 +0200

    made log in button go to main page.

--- app/src/main/java/com/tosp/dreamcatcher/LoginActivity.java
+++ app/src/main/java/com/tosp/dreamcatcher/LoginActivity.java
@@ -1,17 +1,28 @@
 package com.tosp.dreamcatcher;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        username =  findViewById(R.id.username);
+        password  =  findViewById(R.id.password);
+    }
+
+    public void LogInClicked(View view){
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
     }
 
 }
