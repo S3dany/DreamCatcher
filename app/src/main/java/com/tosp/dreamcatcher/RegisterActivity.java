cc0ad5a8c1b6aad476462826133e2ecc0da25627
S3dany <s3dany88@gmail.com>
2020-10-09 16:22:54 +0200

    added needed text views and buttons to Register layout and added the needed functions to the activity.

--- app/src/main/java/com/tosp/dreamcatcher/RegisterActivity.java
+++ app/src/main/java/com/tosp/dreamcatcher/RegisterActivity.java
@@ -1,8 +1,10 @@
 package com.tosp.dreamcatcher;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void signUpClicked(View view){
+        //Signup
+
+    }
+
+    public void signInTextViewClicked(View view){
+        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
