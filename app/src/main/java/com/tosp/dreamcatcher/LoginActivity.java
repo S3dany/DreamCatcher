a3fde6a2ed69c60d99b1b703d5d989b062ed5ddc
S3dany <s3dany88@gmail.com>
2020-10-28 09:45:09 +0100

    added firebase authentication

--- app/src/main/java/com/tosp/dreamcatcher/LoginActivity.java
+++ app/src/main/java/com/tosp/dreamcatcher/LoginActivity.java
@@ -2,27 +2,51 @@ package com.tosp.dreamcatcher;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText username, password;
-
+    EditText email, password;
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        username =  findViewById(R.id.username);
-        password  =  findViewById(R.id.password);
+        email =  findViewById(R.id.emailInLogIn);
+        password  =  findViewById(R.id.passwordInLogIn);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void LogInClicked(View view){
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
+        //Sign in the user
+        mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "Sign in successful", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+
+                } else {
+                    Log.w("ERROR-----------ERROR", task.getException());
+                    Toast.makeText(LoginActivity.this, "Sign in failed. Try Again", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     public void RegisterTextViewClicked(View view){
--- app/src/main/java/com/tosp/dreamcatcher/RegisterActivity.java
+++ app/src/main/java/com/tosp/dreamcatcher/RegisterActivity.java
@@ -1,21 +1,59 @@
 package com.tosp.dreamcatcher;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText username, email, password, confirmPassword;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        email =  findViewById(R.id.emailEditText);
+        username = findViewById(R.id.usernameEditText);
+        password  =  findViewById(R.id.passwordEditText);
+        confirmPassword =  findViewById(R.id.confirmPWeditText);
+        mAuth = FirebaseAuth.getInstance();
+
+        //User signed in
+        if (mAuth.getCurrentUser() != null) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
     }
 
     public void signUpClicked(View view){
-        //Signup
+        //Sign up
+        mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(RegisterActivity.this, "Sign up successful", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    startActivity(intent);
+
+                } else {
+                    Log.w("ERROR-----------ERROR", task.getException());
+                    Toast.makeText(RegisterActivity.this, "Sign up failed. Try Again", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 
