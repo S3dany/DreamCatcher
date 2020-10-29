1241215e6bbc401d822ca1472bd9048a035e3294
S3dany <s3dany88@gmail.com>
2020-10-29 17:56:22 +0100

    added instrumented test suite for all the instrumental tests

---
+++ app/src/androidTest/java/com/tosp/dreamcatcher/InstrumentedTestSuite.java
@@ -0,0 +1,11 @@
+package com.tosp.dreamcatcher;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+// Runs all unit tests.
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        LoginActivityTest.class,
+        RegisterActivityTest.class
+})
+public class InstrumentedTestSuite {}
