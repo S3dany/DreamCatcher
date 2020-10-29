6977046b767f39c4f1924663572985c41fa22255
adrian <itsadrianojog@gmail.com>
2020-10-29 19:30:40 +0100

    Added NewDreamActivityTest to the suite class

--- app/src/androidTest/java/com/tosp/dreamcatcher/InstrumentedTestSuite.java
+++ app/src/androidTest/java/com/tosp/dreamcatcher/InstrumentedTestSuite.java
@@ -6,6 +6,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         LoginActivityTest.class,
-        RegisterActivityTest.class
+        RegisterActivityTest.class,
+        NewDreamActivityTest.class
 })
 public class InstrumentedTestSuite {}
