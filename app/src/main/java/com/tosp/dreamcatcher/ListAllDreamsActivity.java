a9f20d4912b6bccd2aaf689096463bf7659d4e54
Elena Ovsiannikova <elena.ovsiannikova@nokia.com>
2020-12-04 21:46:33 +0100

    dream titles are displayed in list all dreams page, multiple dreams can be written and stored for one user

--- app/src/main/java/com/tosp/dreamcatcher/ListAllDreamsActivity.java
+++ app/src/main/java/com/tosp/dreamcatcher/ListAllDreamsActivity.java
@@ -1,14 +1,56 @@
 package com.tosp.dreamcatcher;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class ListAllDreamsActivity extends AppCompatActivity {
+    ListView listView;
+    FirebaseAuth mAuth;
+    ArrayList<String> data = new ArrayList<>();
+    ArrayAdapter adapter;
+    Integer counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_all_dreams);
+
+        listView = findViewById(R.id.listView);
+        adapter = new ArrayAdapter<String>(this, R.layout.list_item, data);
+        listView.setAdapter(adapter);
+        mAuth = FirebaseAuth.getInstance();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Dreams").child(mAuth.getCurrentUser().getUid());
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                data.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    data.add(snapshot.child("dream_title").getValue().toString());
+                    counter++;
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/tosp/dreamcatcher/NewDreamActivity.java
+++ app/src/main/java/com/tosp/dreamcatcher/NewDreamActivity.java
@@ -1,22 +1,36 @@
 package com.tosp.dreamcatcher;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class NewDreamActivity extends AppCompatActivity {
     EditText dream_title, dream_content;
     FirebaseAuth mAuth;
     DreamEntry newDream = new DreamEntry();
+    Integer counter = 0;
+    public static final String APP_PREFERENCES = "mysettings";
+    String APP_PREFERENCES_COUNTER = "counter";
+
+    SharedPreferences mSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +40,31 @@ public class NewDreamActivity extends AppCompatActivity {
         dream_title = findViewById(R.id.dreamTitle);
         dream_content = findViewById(R.id.dreamInput);
         mAuth = FirebaseAuth.getInstance();
+        mSettings = getSharedPreferences(APP_PREFERENCES, Context.MODE_PRIVATE);
+
+        String user_id = mAuth.getCurrentUser().getUid();
+        DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Dreams").child(user_id);
+
+        current_user_db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+
+                System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                System.out.println(snapshot.getValue());
+                if (snapshot.getValue() == null) {
+                    counter = 0;
+                    SharedPreferences.Editor editor = mSettings.edit();
+                    editor.putInt(APP_PREFERENCES_COUNTER, counter);
+                    editor.apply();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+
+            }
+        });
+
     }
 
     public void saveDreamClicked(View view) {
@@ -35,11 +74,26 @@ public class NewDreamActivity extends AppCompatActivity {
         newDream.setTitle(dream_title.getText().toString());
         newDream.setText(dream_content.getText().toString());
 
+        if(mSettings.contains(APP_PREFERENCES_COUNTER)) {
+            counter = (mSettings.getInt(APP_PREFERENCES_COUNTER, 0));
+        }
+
+        //DatabaseReference users_db = FirebaseDatabase.getInstance().getReference("Users");
+
+        //String text = "";
+        //final Pattern pattern = Pattern.compile("n[0-9]+");
+        //Matcher matcher = pattern.matcher(text);
 
         Map newUser = new HashMap();
-        newUser.put("dream title", newDream.getTitle());
-        newUser.put("dream content", newDream.getText());
+        newUser.put("dream_title", newDream.getTitle());
+        newUser.put("dream_content", newDream.getText());
 
-        current_user_db.setValue(newUser);
+
+        current_user_db.child("n"+ counter).setValue(newUser);
+        //dreamCount.setValue(newUser);
+        counter++;
+        SharedPreferences.Editor editor = mSettings.edit();
+        editor.putInt(APP_PREFERENCES_COUNTER, counter);
+        editor.apply();
     }
 }
